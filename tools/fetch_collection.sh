46c49789e764bcb9a7935533902cd82cf52841fb
Erin <elmtree8@gmail.com>
2017-05-05 15:18:29 -0600

    started fetch_collection. that works right now but word_count, tfidf, and lsi wont work because it pulls all kinds of documents and those all only work on .txt files right now

--- tools/fetch_collection.sh
+++ tools/fetch_collection.sh
@@ -1,1 +1,6 @@
 #!/bin/sh
+my_date() {
+	date "+%Y-%m-%d %T.%N"
+}
+ia download --search "collection:"$1 --destdir=source
+sqlite3 processed/hist.db "INSERT INTO History VALUES('fetch_collection', '"$1"', 'True','$(my_date)')"
