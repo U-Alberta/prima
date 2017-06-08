329335f5bf17c102060051623c216e6538cb283e
Erin <elmtree8@gmail.com>
2017-06-08 16:12:32 -0600

    min_hash now checks for the shingles.db before it tries to start making shingles

--- src/min_hash.py
+++ src/min_hash.py
@@ -14,16 +14,21 @@ def min_hash():
 	if len(sys.argv) != 1:
 		shared.error("11", ["min_hash", ""])
 		return -1
-	try:
-		shingles, docs = shared.gen_shingles()
-	except:
-		shared.error("6", ["min_hash", ""])
-		return -1
-	try:
-		insert_shingles(shingles, docs)
-	except:
-		shared.error("9", ["min_hash", ""], SHINGLEDB)
-		return -1
+	conn = sqlite3.connect(SHINGLEDB)
+	c = conn.cursor()
+	c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name="\
+		"'Shingle'")
+	if c.fetchone() == None:
+		try:
+			shingles, docs = shared.gen_shingles()
+		except:
+			shared.error("6", ["min_hash", ""])
+			return -1
+		try:
+			insert_shingles(shingles, docs)
+		except:
+			shared.error("9", ["min_hash", ""], SHINGLEDB)
+			return -1
 	try:
 		if not os.path.exists(MINHASHFOLDER):
 			os.makedirs(MINHASHFOLDER)
@@ -50,17 +55,16 @@ def insert_shingles(shingles, documents):
 	c = conn.cursor()
 	c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name="\
 		"'Shingle'")
-	if c.fetchone() == None:
-		for i in range(0, len(documents)):
-			documents[i] = (i, documents[i])
-		c.execute("CREATE TABLE Shingle(docid int, shingle text)")
-		conn.commit()
-		c.executemany("INSERT INTO Shingle VALUES(?,?)", shingles)
-		conn.commit()
-		c.execute("CREATE TABLE Document(docid int, docname text)")
-		conn.commit()
-		c.executemany("INSERT INTO Document VALUES(?,?)", documents)
-		conn.commit()
+	for i in range(0, len(documents)):
+		documents[i] = (i, documents[i])
+	c.execute("CREATE TABLE Shingle(docid int, shingle text)")
+	conn.commit()
+	c.executemany("INSERT INTO Shingle VALUES(?,?)", shingles)
+	conn.commit()
+	c.execute("CREATE TABLE Document(docid int, docname text)")
+	conn.commit()
+	c.executemany("INSERT INTO Document VALUES(?,?)", documents)
+	conn.commit()
 	conn.close()
 	return 1
 
